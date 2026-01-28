b6d1e6daa3685ba97af0010837ce5eca9753f5b4
Kanat Sydykov <ksydykov@nurtelecom.kg>
2026-01-28 16:27:01 +0600

    some improves

--- app/page.tsx
+++ app/page.tsx
@@ -2,19 +2,62 @@ import Image from 'next/image';
 
 export default function Page() {
 	return (
-		<div className='mx-auto flex h-screen max-w-lg flex-col justify-center p-4'>
-			<Image
-				src={'/kanat.jpeg'}
-				alt='Kanat'
-				width={300}
-				height={300}
-				className='mb-4 h-auto w-full rounded-lg'
+		<div className='relative flex min-h-svh w-full items-center justify-center'>
+			<div
+				className='absolute inset-0 z-0'
+				style={{
+					backgroundImage: `
+        linear-gradient(to right, #e7e5e4 1px, transparent 1px),
+        linear-gradient(to bottom, #e7e5e4 1px, transparent 1px)
+      `,
+					backgroundSize: '20px 20px',
+					backgroundPosition: '0 0, 0 0',
+					maskImage: `
+        repeating-linear-gradient(
+          to right,
+          black 0px,
+          black 3px,
+          transparent 3px,
+          transparent 8px
+        ),
+        repeating-linear-gradient(
+          to bottom,
+          black 0px,
+          black 3px,
+          transparent 3px,
+          transparent 8px
+        )
+      `,
+					WebkitMaskImage: `
+        repeating-linear-gradient(
+          to right,
+          black 0px,
+          black 3px,
+          transparent 3px,
+          transparent 8px
+        ),
+        repeating-linear-gradient(
+          to bottom,
+          black 0px,
+          black 3px,
+          transparent 3px,
+          transparent 8px
+        )
+      `,
+					maskComposite: 'intersect',
+					WebkitMaskComposite: 'source-in',
+				}}
 			/>
 
-			<p>
-				Hello! I&apos;m Kanat Sydykov, a passionate software developer with a
-				love for creating innovative solutions. Welcome to my personal website!
-			</p>
+			<div className='relative z-50 mt-6 ml-2'>
+				<Image
+					src={'/kanat.jpg'}
+					width={312}
+					height={312}
+					className='size-78 rounded-md object-cover shadow-md'
+					alt='Сыдыков Канат'
+				/>
+			</div>
 		</div>
 	);
 }
